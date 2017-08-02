93af8b22712a407e2b7e49beff74a4d8b189f601
Scholli <scholliyt@gmail.com>
2017-08-02 20:46:45 +0200

    improved performance

--- Code/Code.ino
+++ Code/Code.ino
@@ -39,13 +39,13 @@ and GND.
 #define DEBUGMULTIPLEX 0
 // Matrix Size
 #define MATRIX_ROWS 16
-#define MATRIX_COLUMS 16
+#define MATRIX_COLUMNS 16
 // Shift register PINS
 #define SData 23
 #define SLatch 22
 #define SClock 21
 // DMX Data
-uint8_t data[MATRIX_ROWS * MATRIX_COLUMS * 3];
+uint8_t data[MATRIX_ROWS * MATRIX_COLUMNS * 3];
 // DMX Connection
 namespace teensydmx =::qindesign::teensydmx;
 teensydmx::Receiver dmxRxA
@@ -64,31 +64,32 @@ void setup()
   Serial.print("\n\n\n\n\n");
   Serial.println("Starting LED Matrix");
   Serial.printf("TLCs: %d\n", NUM_TLCS);
-  Serial.printf("Dimension: %dx%d\n", MATRIX_ROWS, MATRIX_COLUMS);
+  Serial.printf("Dimension: %dx%d\n", MATRIX_ROWS, MATRIX_COLUMNS);
   pinMode(SData, OUTPUT);
   pinMode(SLatch, OUTPUT);
   pinMode(SClock, OUTPUT);
   pinMode(13, OUTPUT);
   dmxRxA.begin();
   dmxRxB.begin();
-  for (int i = 0; i < 5; i++)
+  /*for (int i = 0; i < 5; i++)
   {
-    digitalWrite(13, HIGH);
-    delay(50);
-    digitalWrite(13, LOW);
-    delay(50);
-  }
-  delay(500);
   digitalWrite(13, HIGH);
-  delay(500);
-  /* Call Tlc.init() to setup the tlc.
-  You can optionally pass an initial PWM value (0 - 4095) for all channels.*/
-  Tlc.init();
+  delay(50);
+  digitalWrite(13, LOW);
+  delay(50);
+}*/
+digitalWrite(13, HIGH);
+delay(500);
+digitalWrite(13, LOW);
+delay(500);
+digitalWrite(13, HIGH);
+/* Call Tlc.init() to setup the tlc.
+You can optionally pass an initial PWM value (0 - 4095) for all channels.*/
+Tlc.init();
 }
 
 void loop()
 {
-
 
   #if DEBUGDMX
   long micGetDMX = micros();
@@ -100,36 +101,32 @@ void loop()
   Serial.printf("Got new DMX Values. Time µs: %d\n", micros() - micGetDMX);
   #endif
 
-
-  for (uint8_t row = 0; row < MATRIX_ROWS; ++row)
+  for (int row = 0; row < MATRIX_ROWS; ++row)
   {
-
 
     #if DEBUGMULTIPLEX
     long micTlc = micros();
     #endif
 
-
-    for (uint8_t colum = 0; colum < MATRIX_COLUMS; ++colum)
+    Tlc.clear();
+    for (int column = 0; column < MATRIX_COLUMNS; ++column)
     {
 
-
       #if DEBUGVALUES
-      Serial.printf("[%d %d %d] ", getVlaue(row, colum, 0), getVlaue(row, colum, 1), getVlaue(row, colum, 2));
+      Serial.printf("[%d %d %d] ", getValue(row, column, 0), getValue(row, column, 1), getValue(row, column, 2));
       #endif
 
-
       // Red
-      Tlc.set(colum, getVlaue(row, colum, 0) * 16);
+      
+      Tlc.set(column, data[row * 16 * 3 + column * 3 + 0]);
       // Green
-      Tlc.set(colum + MATRIX_COLUMS, getVlaue(row, colum, 1) * 16);
+      Tlc.set(column + MATRIX_COLUMNS, data[row * 16 * 3 + column * 3 + 1] * 16);
       // Blue
-      Tlc.set(colum + MATRIX_COLUMS * 2, getVlaue(row, colum, 2) * 16);
+      Tlc.set(column + MATRIX_COLUMNS * 2, data[row * 16 * 3 + column * 3 + 2] * 16);
     }
 
-
     #if DEBUGMULTIPLEX
-    Serial.printf("Tlc Values for all colums. Time µs: %d\n", micros() - micTlc);
+    Serial.printf("Tlc Values for all columns. Time µs: %d\n", micros() - micTlc);
     #endif
 
     #if DEBUGVALUES
@@ -139,7 +136,7 @@ void loop()
     #if DEBUGMULTIPLEX
     long micOutput = micros();
     #endif
-
+    clearMultiplex();
     multiplex(row);
 
     #if DEBUGMULTIPLEX
@@ -153,31 +150,14 @@ void loop()
     Serial.printf("Update Tlc. Time µs: %d\n", micros() - micOutput);
     #endif
 
-
-    delayMicroseconds(100);
-    //delay(100);
-
-    digitalWrite(SLatch, LOW); // Latch Low
-    ShiftOut(0);
-    ShiftOut(0);
-    Tlc.clear();
-    Tlc.update();
-    delayMicroseconds(1);
-    digitalWrite(SLatch, HIGH); // Latch High
+    //delayMicroseconds(100000);
+    // delay(100);
   }
-
 
   #if DEBUGVALUES
   Serial.print("\n\n\n");
   #endif
 
-
-}
-
-// color: 0->RED, 1->GREEN, 2->BLUE
-int getVlaue(uint8_t row, uint8_t colum, uint8_t color)
-{
-  return data[row * 16 * 3 + colum * 3 + color];
 }
 
 void getDMX()
@@ -187,16 +167,13 @@ void getDMX()
   if (readA == 510)
   {
 
-
     #if DEBUG
     Serial.printf("DMX.A: %d\n", readA);
     #endif
 
-
     memcpy(data, tempA, sizeof(tempA));
   }
   else
-
   if (readA == -1)
   {
 
@@ -205,7 +182,6 @@ void getDMX()
     #endif
 
   }
-
   else
   {
     Serial.printf("%s: %d\n", "Error reading all 510 required DMX Channels from DMX.A. Read: ", readA);
@@ -215,16 +191,13 @@ void getDMX()
   if (readB == 258)
   {
 
-
     #if DEBUG
     Serial.printf("DMX.B: %d\n", readB);
     #endif
 
-
     memcpy(data + 510, tempB, sizeof(tempB));
   }
   else
-
   if (readB == -1)
   {
 
@@ -233,56 +206,46 @@ void getDMX()
     #endif
 
   }
-
   else
   {
     Serial.printf("%s: %d\n", "Error reading all 258 required DMX Channels from DMX.A. Read: ", readB);
   }
 }
 
-void multiplex(uint8_t row)
+void multiplex(int row)
 {
-  digitalWrite(SLatch, LOW); // Latch Low
-  uint16_t value = 1 << row;
-  ShiftOut(value);
-  ShiftOut(value >> 8);
-  delayMicroseconds(1);
-  digitalWrite(SLatch, HIGH); // Latch High
+  digitalWriteFast(SLatch, LOW); // Latch Low
+  uint16_t shift_data = 1 << row;
+  //ShiftOut(shift_data);
+  // ============================= SHIFTOUT =============================
+  for (uint16_t i = 0; i < 16; i++)
+  {
+    digitalWriteFast(SData, !!(shift_data & (1 << i)));
+    digitalWriteFast(SClock, HIGH);
+    __asm__ __volatile__ ("nop\n\t");
+    __asm__ __volatile__ ("nop\n\t");
+    digitalWriteFast(SClock, LOW);
+  }
+  digitalWriteFast(SData, LOW);
+  // ============================= SHIFTOUT =============================
+  digitalWriteFast(SLatch, HIGH); // Latch High
 }
 
-void ShiftOut(uint8_t myDataOut)
+void clearMultiplex()
 {
-  // This shifts 8 bits out lngMsLoopstartB first,
-  // on the rising edge of the clock,
-  // clock idles low
-  // internal function setup
-  // clear everything out just in case to
-  // prepare shift register for bit shifting
-  digitalWrite(SData, LOW); // Data aus
-  digitalWrite(SClock, LOW); // Clock aus
-  // for each bit in the uint8_t myDataOutï
-  // NOTICE THAT WE ARE COUNTING DOWN in our for loop
-  // This means that %00000001 or "1" will go through such
-  // that it will be pin Q0 that lights.
-  for (uint8_t i = 0; i <= 7; i++)
+  digitalWriteFast(SLatch, LOW); // Latch Low
+  uint16_t shift_data = 0;
+  //ShiftOut(shift_data);
+  // ============================= SHIFTOUT =============================
+  for (uint16_t i = 0; i < 16; i++)
   {
-    digitalWrite(SClock, LOW); // Clock aus
-    // if the value passed to myDataOut and a bitmask result
-    // true then... so if we are at i=6 and our value is
-    // %11010100 it would the code compares it to %01000000
-    // and proceeds to set pinState to 1.
-    if (myDataOut & (1 << i))
-    {
-      digitalWrite(SData, HIGH); // Data an
-    }
-    else
-    {
-      digitalWrite(SData, LOW); // Data aus
-    }
-    // register shifts bits on upstroke of clock pin
-    digitalWrite(SClock, HIGH); // Clock an
-    digitalWrite(SClock, LOW); // Clock aus
-    // zero the data pin after shift to prevent bleed through
-    digitalWrite(SData, LOW); // Data aus
+    digitalWriteFast(SData, !!(shift_data & (1 << i)));
+    digitalWriteFast(SClock, HIGH);
+    __asm__ __volatile__ ("nop\n\t");
+    __asm__ __volatile__ ("nop\n\t");
+    digitalWriteFast(SClock, LOW);
   }
+  digitalWriteFast(SData, LOW);
+  // ============================= SHIFTOUT =============================
+  digitalWriteFast(SLatch, HIGH); // Latch High
 }
---
+++ ShiftRegisterTest/ShiftRegisterTest.ino
@@ -0,0 +1,74 @@
+#define SData 23
+#define SLatch 22
+#define SClock 21
+
+void setup() {
+  pinMode(SData, OUTPUT);
+  pinMode(SLatch, OUTPUT);
+  pinMode(SClock, OUTPUT);
+  digitalWrite(SData, LOW);
+  digitalWrite(SLatch, LOW);
+  digitalWrite(SClock, LOW);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+  for (uint8_t row = 0; row < 16; ++row)
+  {
+
+    multiplex(row);
+    delay(1);
+}
+
+	
+}
+
+void multiplex(uint8_t row)
+{
+  digitalWrite(SLatch, LOW); // Latch Low
+  uint16_t value = 1 << row;
+  ShiftOut(value);
+  ShiftOut(value >> 8);
+  digitalWrite(SLatch, HIGH); // Latch High
+}
+
+void ShiftOut(uint8_t myDataOut)
+{
+  // This shifts 8 bits out lngMsLoopstartB first,
+  // on the rising edge of the clock,
+  // clock idles low
+  // internal function setup
+  // clear everything out just in case to
+  // prepare shift register for bit shifting
+  digitalWrite(SData, LOW); // Data aus
+  digitalWrite(SClock, LOW); // Clock aus
+  // for each bit in the uint8_t myDataOutï
+  // NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  // This means that %00000001 or "1" will go through such
+  // that it will be pin Q0 that lights.
+  for (uint8_t i = 0; i <= 7; i++)
+  {
+    delayMicroseconds(1);
+    digitalWrite(SClock, LOW); // Clock aus
+    // if the value passed to myDataOut and a bitmask result
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000
+    // and proceeds to set pinState to 1.
+    if (myDataOut & (1 << i))
+    {
+      digitalWrite(SData, HIGH); // Data an
+    }
+    else
+    {
+      digitalWrite(SData, LOW); // Data aus
+    }
+    delayMicroseconds(1);
+    // register shifts bits on upstroke of clock pin
+    digitalWrite(SClock, HIGH); // Clock an
+    delayMicroseconds(1);
+    digitalWrite(SClock, LOW); // Clock aus
+    // zero the data pin after shift to prevent bleed through
+    digitalWrite(SData, LOW); // Data aus
+  }
+}
